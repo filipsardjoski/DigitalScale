8f4e05ddf819a5ec092427fcf57492afcc008e5c
Filip Sardzoski <f.sardzoski@gmail.com>
2017-07-20 00:19:11 +0200

    Initial Commit

--- DigitalScale/ViewController.swift
+++ DigitalScale/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var forceLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        if let touch = touches.first {
+            if traitCollection.forceTouchCapability == UIForceTouchCapability.available {
+                if touch.force >= touch.maximumPossibleForce {
+                    forceLabel.text = "385+ grams"
+                } else {
+                    let force = touch.force/touch.maximumPossibleForce
+                    let grams = force * 385
+                    let roundGrams = Int(grams)
+                    forceLabel.text = "\(roundGrams) grams"
+                }
+            }
+        }
     }
-
-
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        forceLabel.text = "0 grams"
+    }
+    
 }
 
+
+
+
+
+// check for 3D Touch
+/*
+ func is3DTouchAvailable() -> Bool {
+ return self.traitCollection.forceTouchCapability == UIForceTouchCapability.Available
+ }
+ */
